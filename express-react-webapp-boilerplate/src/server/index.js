3f3123071e1bba53e1c3b97cb7b7e734a9817a61
pau1m <paul@metagroove.io>
2020-08-07 16:18:46 +0100

    amending acceptable http verbs and switching port

--- express-react-webapp-boilerplate/src/server/index.js
+++ express-react-webapp-boilerplate/src/server/index.js
@@ -4,7 +4,7 @@ const CakeRouter = require('./cakes/routes.config.js');
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST', 'PATCH', 'DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET, POST', 'PUT', /*patch*/ 'DELETE');
   res.header('Access-Control-Expose-Headers', 'Content-Length');
   res.header('Access-Control-Allow-Headers', 'Accept, Content-Type, X-Requested-With, Range');
   if (req.method === 'OPTIONS') {
@@ -20,4 +20,4 @@ app.use(express.json());
 
 CakeRouter.routesConfig(app);
 
-app.listen(1337, () => console.log('Listening on port 1337'))
+app.listen(1338, () => console.log('Listening on port 1338'))
