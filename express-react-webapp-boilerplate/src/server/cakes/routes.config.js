bb1dc8135b7fd8c256d3c4097cce48a39c2e1b20
pau1m <paul@metagroove.io>
2020-08-07 11:49:32 +0100

    fixing up routes so they all return something

--- express-react-webapp-boilerplate/src/server/cakes/routes.config.js
+++ express-react-webapp-boilerplate/src/server/cakes/routes.config.js
@@ -9,28 +9,32 @@ exports.routesConfig = async function (app) {
     yumFactor: 1
   }
 
-  // todo prefix api/v1 to all
+  // todo prefix api/v1 to alla
 
   app.get('/api/v1/cakes', (req, res) => {
     console.log('hello world');
+    res.send(
+      [aCake, aCake, aCake]
+    )
+  })
+
+  app.get('/api/v1/cakes/:id', (req, res) => {
+    console.log('id fetched');
     res.send(
       aCake
     )
   })
 
-  app.get('/api/v1/cakes/{id}', (req, res) => res.send(
-    [aCake, aCake, aCake]
-  ))
-
-  app.post('/api/v1/cakes', (req, res) => res.send(
+  app.post('/api/v1/cakes', (req, res) => res.status(201).send(
     aCake
   ))
 
+  // id?
   app.put('/api/v1/cakes', (req, res) => res.send(
     aCake
   ))
 
-  app.delete('/api/v1/cakes', (req, res) => res.status(
+  app.delete('/api/v1/cakes/:id', (req, res) => res.status(
     200
-  ))
+  ).send())
 }
