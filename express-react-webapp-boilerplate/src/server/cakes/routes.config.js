33bdabd18180989a24f11bd8cc2a374431709267
pau1m <paul@metagroove.io>
2020-08-07 10:56:45 +0100

    adding basic routes and amending headers

--- express-react-webapp-boilerplate/src/server/cakes/routes.config.js
+++ express-react-webapp-boilerplate/src/server/cakes/routes.config.js
@@ -1,1 +1,36 @@
-//touch
+
+exports.routesConfig = async function (app) {
+
+  const aCake = {
+    id: 1,
+    name: 'foo',
+    comment: 'boo',
+    imageUrl: 'moo',
+    yumFactor: 1
+  }
+
+  // todo prefix api/v1 to all
+
+  app.get('/api/v1/cakes', (req, res) => {
+    console.log('hello world');
+    res.send(
+      aCake
+    )
+  })
+
+  app.get('/api/v1/cakes/{id}', (req, res) => res.send(
+    [aCake, aCake, aCake]
+  ))
+
+  app.post('/api/v1/cakes', (req, res) => res.send(
+    aCake
+  ))
+
+  app.put('/api/v1/cakes', (req, res) => res.send(
+    aCake
+  ))
+
+  app.delete('/api/v1/cakes', (req, res) => res.status(
+    200
+  ))
+}
--- express-react-webapp-boilerplate/src/server/index.js
+++ express-react-webapp-boilerplate/src/server/index.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-//const CakeRouter = require('cakes/routes.config.js');
+const CakeRouter = require('./cakes/routes.config.js');
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*'); // it's just a test
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST', 'PATCH', 'DELETE');
   res.header('Access-Control-Expose-Headers', 'Content-Length');
+  res.header('Access-Control-Allow-Headers', 'Accept, Content-Type, X-Requested-With, Range');
+  if (req.method === 'OPTIONS') {
+    return res.send(200);
+  } else {
+    return  next();
+  }
 });
+
 app.use(express.static('dist'))
 app.use(bodyParser.json());
-//CakeRouter.routesConfig(app);
 
-app.listen(1338, () => console.log('Listening on port 1337, API on port 1338!'))
+CakeRouter.routesConfig(app);
+
+app.listen(1337, () => console.log('Listening on port 1337'))
