1778246600aafff36b4883782eab57827f0c1434
pau1m <paul@metagroove.io>
2020-08-07 16:19:26 +0100

    tidying up routes file

--- express-react-webapp-boilerplate/src/server/cakes/routes.config.js
+++ express-react-webapp-boilerplate/src/server/cakes/routes.config.js
@@ -2,16 +2,6 @@ const CakeController = require('../cakes/controller/cakes.controller')
 
 exports.routesConfig = async function (app) {
 
-  const aCake = {
-    id: 1,
-    name: 'foo',
-    comment: 'boo',
-    imageUrl: 'moo',
-    yumFactor: 1
-  }
-
-  // todo prefix api/v1 to alla
-
   app.get('/api/v1/cakes', [CakeController.fetchAll])
 
   app.get('/api/v1/cakes/:id', [CakeController.getById])
